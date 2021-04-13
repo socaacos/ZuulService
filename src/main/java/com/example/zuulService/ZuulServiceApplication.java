0070b6ede79b8834c753a6053272cb813100b936
sofija jakovljevic <sofija96j@gmail.com>
2021-04-13 13:56:46 +0200

    Zuul service

---
+++ src/main/java/com/example/zuulService/ZuulServiceApplication.java
@@ -0,0 +1,43 @@
+package com.example.zuulService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.example.zuulService.filter.ErrorFilter;
+import com.example.zuulService.filter.PostFilter;
+import com.example.zuulService.filter.PreFilter;
+import com.example.zuulService.filter.RouteFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
